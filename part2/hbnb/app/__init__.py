c955149b91f9340fdd5d766dbbf3ef718593692d
mAgAtSuRi <tristan.latuilliere@gmail.com>
2025-10-27 13:17:49 +0100

    task 0 p3

--- part2/hbnb/app/__init__.py
+++ part2/hbnb/app/__init__.py
@@ -4,10 +4,12 @@ from .api.v1.users import api as users_ns
 from .api.v1.amenities import api as amenities_ns
 from .api.v1.places import api as places_ns
 from .api.v1.reviews import api as reviews_ns
+from config import config
 
 
-def create_app():
+def create_app(config_name="development"):
     app = Flask(__name__)
+    app.config.from_object(config[config_name])
     api = Api(app, version='1.0', title='HBnB API',
               description='HBnB Application API', doc='/api/v1/')
 
